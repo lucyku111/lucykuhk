91ad18bcb3e11e5536c0156b8d06ff499af040b0
lucyku111 <lucypro143@gmail.com>
2025-04-02 21:09:55 +0530

    Fix syntax error in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove output: 'export' for Netlify deployment
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -14,7 +15,8 @@ const nextConfig = {
       "ext.same-assets.com",
       "ugc.same-assets.com",
     ],
-  },
+  },  // Added comma here
+  // Removed experimental.esmExternals as it's not supported by Turbopack
 };
 
 module.exports = nextConfig;
